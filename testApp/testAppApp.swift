902e7811e966aa3c04332dd0e7cffb108c82e95b
spoons <ev4n.mus1c@gmail.com>
2024-01-29 08:03:27 -0700

    Initial Commit

---
+++ testApp/testAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  testAppApp.swift
+//  testApp
+//
+//  Created by shark boy on 1/29/24.
+//
+
+import SwiftUI
+
+@main
+struct testAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
